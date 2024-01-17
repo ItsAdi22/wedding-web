a634752f61f95bed1d99a8e03e8622413d2ac4b1
Aditya Mondkar <adityamondkar111@gmail.com>
2024-01-17 23:59:08 +0530

    added signup page and route

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template,redirect ,url_for
 
 app = Flask(__name__)
 
-@app.get('/')
+@app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('login/signup.html')
+
 app.run(port=80,debug=True)
+
