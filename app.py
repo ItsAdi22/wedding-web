0dabb88b69a4ee531aedfae004341cd3b64f8f06
Aditya Mondkar <adityamondkar111@gmail.com>
2024-01-18 00:47:03 +0530

    backend and frontend connection done for signup page

--- app.py
+++ app.py
@@ -1,18 +1,33 @@
-from flask import Flask, render_template,redirect ,url_for
+from flask import Flask, render_template,redirect ,url_for, request
+from forms import SignupForm
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/signup')
+@app.route('/signup',methods=['GET','POST'])
 def signup():
-    return render_template('login/signup.html')
+    form = SignupForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            userName = request.form.get("userName")
+            email = request.form.get("email")
+            password = request.form.get("password")
+            print(f'{userName} -<>- {email} -<>- {password}')
+            return redirect(url_for('home'))
+
+    else:
+        return render_template('login/signup.html',form=SignupForm())
 
 @app.route('/login')
 def login():
-    return render_template('login/login.html')
+    return render_template('login/login.html',form=SignupForm())
 
 app.run(port=80,debug=True)
 
---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+
+class SignupForm(FlaskForm):
+    userName = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Sign Up')
